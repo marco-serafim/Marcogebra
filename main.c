d3d341f6b58a767938d8aeeb5cc51f3c5ab523b3
Tylogree <Tylogree>
2022-11-14 10:26:25 -0300

    minor changes

--- main.c
+++ main.c
@@ -2,95 +2,104 @@
 #include "supportLib.h"
 #include <math.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include "functions.h"
+
 
 int main(){
 	FILE* fp;
-	fp = fopen("jorge.txt", "w");
+	fp = fopen("output.txt", "w"); // here comes the file you want to write to
 	fprintf(fp, " ");
 	fclose(fp);
-	fp = fopen("jorge.txt", "a");
+	fp = fopen("output.txt", "a");
 	
-	// setting X and Y values.
-    double x[9999];
-    double y[9999];
 
-    // looping to function
-   	double range_final = 40;                  // FINAL
-	double variable_initial_index = -40;    // INITIAL
-	double resolution = 0.01;                // ACCURACY (less, more accurate)
-    	int list_index = 0;
-
-
-    double xf;
-    double final;
-    
-    // f(x)=b*a^(x+c) + d 
-
-
-    double modulum(double numba){
-        if(numba < 0){
-            numba = numba * -1;
-        }
-        return numba;
-    }
-
-    //	function calculation
-    fprintf(fp, "MarcoGebra | ACCURACY: %lf\n\n ", resolution);
-    for(; variable_initial_index < range_final; variable_initial_index = variable_initial_index + resolution){
-	x[list_index] = variable_initial_index;
-	y[list_index] = pow(variable_initial_index, 2);
-	fprintf(fp, "x: %lf | y: %lf\n", x[list_index], y[list_index]);
-    	list_index++;
-    }
-
-    fprintf(fp, "\n<%d> results \n", list_index);
+	//
+	// FUNCTION CONFIGURATION
+	//
 	
-	fclose(fp);
-    // pbPlots:
+	int list_index = 0;
 
-    // Line:
-    ScatterPlotSeries *line = GetDefaultScatterPlotSeriesSettings();
+    	double x[9999];	// will contain all the x values
+    	double y[9999];	// will contain all the y values
 
-    // Line details and settings:
-    line->xs = x;
-    line->ys = y;
-    line->xsLength = list_index;
-    line->ysLength = list_index;
-    line->linearInterpolation = true;
+   	double range_final = 10; // final x value               
+	double xv = 0; // initial x value:
+	double resolution = 0.01; // resolution (smaller, more precise), default 0.01               
 
-    // Background:
-    ScatterPlotSettings *background = GetDefaultScatterPlotSettings();
+    	for(; xv < range_final; xv += resolution)	// function calculation
+	{
+		x[list_index] = xv; // Getting the x value for a certain index
+		y[list_index] = pow(xv, 2); // here comes your function (same index as x's)
+	
+		// writing to the text file
 
-    // Background settings and details
-    background->autoPadding = true;
-    background->yAxisAuto = true;
-    background->autoBoundaries = true;
-    background->showGrid =  true;
-    background->xAxisBottom = true;
-    background->height = 800;
-    background->width = 1200;
-    background->title = L"MarcoGebra >> Geogebra.";
-    background->titleLength = wcslen(background->title);
+		fprintf(fp, "x: %lf | y: %lf\n", x[list_index], y[list_index]);    	
+		
+		list_index++; // updating index value
+    	}
 
-    // Relacting the Line with the Background
-    ScatterPlotSeries *s [] = {line};
-    background->scatterPlotSeries = s;
+	fprintf(fp, "\n<%d> results \n", list_index); // showing up how many results it got
+	fclose(fp); // close the text file
+
+	//
+	// END OF FUNCTION CONFIGURATION
+	//
+
+
+	//
+	// IMAGE PROCESSING:
+	//
+
+	// pbPlots:
+
+    	// Line:
+    	ScatterPlotSeries *line = GetDefaultScatterPlotSeriesSettings();
+
+    	// Line details and settings:
+    	line->xs = x;
+	line->ys = y;
+    	line->xsLength = list_index;
+    	line->ysLength = list_index;
+    	line->linearInterpolation = true;
+
+    	// Background:
+    	ScatterPlotSettings *background = GetDefaultScatterPlotSettings();
+
+    	// Background settings and details
+    	background->autoPadding = true;
+    	background->yAxisAuto = true;
+    	background->autoBoundaries = true;
+    	background->showGrid =  true;
+    	background->xAxisBottom = true;
+
+	// SIZE IN PIXELS
+    	background->height = 900;
+    	background->width = 1600;	
+    	// SIZE IN PIXELS
+
+	background->title = L"MarcoGebra";
+    	background->titleLength = wcslen(background->title);
+	
+	// Relacting the Line with the Background
+	ScatterPlotSeries *s [] = {line};
+	background->scatterPlotSeries = s;
 	background->scatterPlotSeriesLength = 1;
 
     
-    // Exporting:
+    	// Exporting:
     
-    // Creating a image in the ram
-    RGBABitmapImageReference *ram_image = CreateRGBABitmapImageReference();
+    	// Creating a image in the ram
+    	RGBABitmapImageReference *ram_image = CreateRGBABitmapImageReference();
     
-    // Uploading all previous settings to the ram's image
-    DrawScatterPlotFromSettings(ram_image, background, ((void *)0));
-    size_t length;
-	double *ram_png = ConvertToPNG(&length, ram_image->image);
-	WriteToFile(ram_png, length, "final.png");
-    DeleteImage(ram_image->image);
+    	// Uploading all previous settings to the ram's image
+    	DrawScatterPlotFromSettings(ram_image, background, ((void *)0));
+    	size_t length;
+		double *ram_png = ConvertToPNG(&length, ram_image->image);
+		WriteToFile(ram_png, length, "final.png");
+    	DeleteImage(ram_image->image);
 
-    return 0;
+	//
+	// END OF IMAGE PROCESSING
+	//
+
+	return 0;
 }
